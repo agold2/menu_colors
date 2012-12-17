9f0315eb7673606cb42c9f7a5584858e9b16a3e8
Ariel Gold <agold2@asu.edu>
2012-12-17 14:08:42 -0700

    Fix navbar spacing gaps

--- js/menu_colors.js
+++ js/menu_colors.js
@@ -1,102 +1,34 @@
 Drupal.behaviors.myModule = function(context) {
+  var links = $(".view-landing-page-menu-blockreference .item-list div.block-menu ul li a").not('.view-landing-page-menu-blockreference .item-list div.block-menu ul li ul li a');
 
+  distributeCategoryLinks('.view-landing-page-menu-blockreference .item-list', links);
 
-
-var links = $(".view-landing-page-menu-blockreference .item-list div.block-menu ul li a").not('.view-landing-page-menu-blockreference .item-list div.block-menu ul li ul li a');
-
-distributeCategoryLinks('.view-landing-page-menu-blockreference .item-list', links);
-
-function distributeCategoryLinks(container, links) 
-{
-  var containerWidth = $(container).width();
-  containerWidth = containerWidth - 23; 
-  var listWidth = 0;
-  var j = 0;
-  $(links).each( function () {
-    listWidth = listWidth + $(this).width();
-    if(listWidth > 1000) 
-    {   
-      listWidth = listWidth-$(this).width();
-      $(this).addClass("nextline");
-    }   
-    else 
-    {   
-      j++;
-    }   
-  } );
-  var distance = containerWidth - listWidth;
-  var i = 0;
-  var k = $(links).length - j;
-  var myvalue = [];
-  var mlid = [];
-  $(links).each( function () {
-    if(j > 0)  
-    {   
-      var oneDistance = Math.floor( distance/(2*($(links).length - i - k)));
-      
-      distance = distance - 2*oneDistance;
-      i++;
-      j--;
-      if(j == 0 && oneDistance > 0)
-      {   
-        oneDistance = oneDistance - 3;
-      }   
-      $(this).css("padding-left", oneDistance);
-      $(this).css("padding-right", oneDistance);
-      var classes = $(this).attr('class');
-      classes = classes.split(" ");
-      var l = 0;
-      $(classes).each( function () {
-        if (classes[l].indexOf("menu") != -1) {
-          mlid[i-1] = classes[l];
-        }   
-        l++;
-      } );
-      if (isFinite(oneDistance)) { 
-        myvalue[i] = oneDistance;
+  function distributeCategoryLinks(container, links) {
+    var containerWidth = $(container).width();
+    containerWidth = containerWidth - 23;
+    var linkWidths = 0,
+      linkWidth = new Array(),
+       j = 0;
+    $(links).each( function () {
+      linkWidth[j] = $(this).width();
+      linkWidths = linkWidths + linkWidth[j];
+      if(linkWidths > 1000) {
+        linkWidths = linkWidths - $(this).width();
+        $(this).addClass("nextline");
       }
-      /*if(j > 1)
-        $(this).css("padding-right", oneDistance);
-      else
-        $(this).css("padding-right", oneDistance-1);*/
-    }   
-  } );
-  i--;
-  $(links).each( function () {
-    if(i > 0)
-    {   
-      if($(this).hasClass("nextline"))
-      {   
-        $(this).removeClass("nextline");
-      }   
-      i--;
-    }   
-  } );
-  myvalue = $.merge(myvalue, mlid);
-  //myvalue = myvalue.join('&');
-
-  return myvalue;
-/*  if($(container).height() <= 40)
-  {
-    */
-  $(container).css("height", 40);
-    /*  
+      else {
+        j++;
+      }
+    } );
+    $(links).each ( function () {
+      linkPercent = ($(this).width() / linkWidths) * 100;
+      $(this).css("display", "block");
+      $(this).css("text-align", "center");
+      $(this).css("float", "left");
+      $(this).css("width", linkPercent + '%');
+    } );
+  $("#content").css("clear", "both");
   }
-  else
-  {
-    $(container).css("height", $(container).height()+48);
-  }*/
-  if(k > 0)
-  {
-    distributeCategoryLinks(".view-landing-page-menu .item-list",".view-landing-page-menu .item-list li.nextline");
-  }
-}
-
-/*
-x = '/'+x;
-
-$.get(Drupal.settings.basePath+'my_custom_callback'+x);
-*/
 }
 
 
